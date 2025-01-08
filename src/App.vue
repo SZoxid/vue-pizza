ca1c9e270b12aa5beeb878af7e5ce81b744071f7
SZoxid <zoxidsiddikov@gmail.com>
2025-01-08 16:32:04 +0500

    first2

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-[1340px] m-auto border border-red-500">
+    <header>
+      <nav></nav>
+    </header>
+    <main></main>
+    <footer></footer>
+    <h1 class="text-red-500">hello world</h1>
+  </div>
+</template>
+
+<script setup></script>
+<style scoped></style>
